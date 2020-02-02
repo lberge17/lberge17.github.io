a34fcf7456dc59059d2cfc54f0d3cd255135328d
Laura Berge <lauraeberge@gmail.com>
2020-02-02 16:41:07 -0500

    begins building php mail contact form

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $formcontent="From: $name \n Message: $message";
+    $recipient = "lauraeberge@gmail.com";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+
+    if ($_POST['submit']) {
+        if (mail ($to, $subject, $body, $from)) {
+            echo '<p>Your message has been sent!</p>';
+        } else {
+            echo '<p>Something went wrong, go back and try again!</p>';
+        }
+    }
+?>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     addScroll();
+    listenContact();
 })
 
 function addScroll () {
@@ -7,5 +8,18 @@ function addScroll () {
     const projects = document.querySelector('.projects');
     projectsBtn.addEventListener('click', () => {
         window.scrollTo({ top: projects.offsetTop, behavior: 'smooth' });
+    })
+}
+
+function listenContact() {
+    const contactBtn = document.querySelector('.contact-btn');
+    const popupContainer = document.getElementById('popupContainer');
+    contactBtn.addEventListener('click', () => {
+        popupContainer.classList.remove('hidden');
+    })
+
+    const closeBtn = document.getElementById('close-popup');
+    closeBtn.addEventListener('click', () => {
+        popupContainer.classList.add('hidden');
     })
 }
